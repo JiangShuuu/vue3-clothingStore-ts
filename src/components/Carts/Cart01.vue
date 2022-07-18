7fd5ad5abbfd1dde7f054514af50c918f2c230a9
JiangShuuu <globelex65@gmail.com>
2022-07-18 21:36:03 +0800

    rwd: cart01cart

--- src/components/Carts/Cart01.vue
+++ src/components/Carts/Cart01.vue
@@ -7,28 +7,66 @@
         <p>購物車</p>
         <p>(1 件)</p>
       </div>
-      <div class="flex justify-between p-2">
-        <div class="flex space-x-2">
-          <div class="w-16 h-16 overflow-hidden border">
-            <img
-              src="https://images.unsplash.com/photo-1657879005446-fd4563beddb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-              class="object-cover" alt="">
+      <section class="relative flex-col hidden md:flex">
+        <div class="hidden grid-flow-row grid-cols-5 py-3 text-sm border md:grid">
+          <p>商品資料</p>
+          <p>優惠</p>
+          <p>單件價格</p>
+          <p>數量</p>
+          <p>小計</p>
+        </div>
+        <div class="hidden grid-flow-row grid-cols-5 p-2 text-sm border md:grid">
+          <div class="space-x-2 flex-center">
+            <div class="w-16 h-16 overflow-hidden border">
+              <img
+                src="https://images.unsplash.com/photo-1657879005446-fd4563beddb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                class="object-cover" alt="">
+            </div>
+            <p class="text-sm">防風上衣</p>
           </div>
-          <p class="text-sm">防風上衣</p>
+          <div></div>
+          <div class="flex-center">
+            <p>NT$450</p>
+          </div>
+          <div class="flex-center">
+            <div class="justify-between w-32 p-2 border flex-center">
+              <Icon icon="fa-solid:minus" class="w-4 h-4 cursor-pointer" />
+              <p class="text-sm">2</p>
+              <Icon icon="fa-solid:plus" class="w-4 h-4 cursor-pointer" />
+            </div>
+          </div>
+          <div class="flex-center">
+            <p>NT$900</p>
+          </div>
+          <div class="absolute right-5 top-[4.8rem]">
+            <Icon icon="icon-park-outline:delete-five" class="w-5 h-5 text-gray-500 cursor-pointer" />
+          </div>
         </div>
-        <div class="flex flex-col items-end justify-between text-sm">
-          <Icon icon="icon-park-outline:delete-five" class="w-5 h-5 text-gray-500 cursor-pointer" />
-          <p>NT$450</p>
+      </section>
+      <section class="md:hidden">
+        <div class="flex justify-between p-2">
+          <div class="flex space-x-2">
+            <div class="w-16 h-16 overflow-hidden border">
+              <img
+                src="https://images.unsplash.com/photo-1657879005446-fd4563beddb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                class="object-cover" alt="">
+            </div>
+            <p class="text-sm">防風上衣</p>
+          </div>
+          <div class="flex flex-col items-end justify-between text-sm">
+            <Icon icon="icon-park-outline:delete-five" class="w-5 h-5 text-gray-500 cursor-pointer" />
+            <p>NT$450</p>
+          </div>
         </div>
-      </div>
-      <div class="justify-between p-2 flex-center">
-        <div class="justify-between w-32 p-2 border flex-center">
-          <Icon icon="fa-solid:minus" class="w-4 h-4 cursor-pointer" />
-          <p class="text-sm">2</p>
-          <Icon icon="fa-solid:plus" class="w-4 h-4 cursor-pointer" />
+        <div class="justify-between p-2 flex-center">
+          <div class="justify-between w-32 p-2 border flex-center">
+            <Icon icon="fa-solid:minus" class="w-4 h-4 cursor-pointer" />
+            <p class="text-sm">2</p>
+            <Icon icon="fa-solid:plus" class="w-4 h-4 cursor-pointer" />
+          </div>
+          <p>NT$900</p>
         </div>
-        <p>NT$900</p>
-      </div>
+      </section>
     </section>
     <section class="border">
       <div class="flex p-2 pl-4 space-x-3 text-lg bg-gray-200 text-start">
