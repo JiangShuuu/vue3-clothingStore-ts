a681c028eb2511856942dca06977e6b44c023007
JiangShuuu <globelex65@gmail.com>
2022-07-23 15:18:57 +0800

    add Cart03

--- src/components/Carts/Cart03.vue
+++ src/components/Carts/Cart03.vue
@@ -1,5 +1,11 @@
 <template>
-  <p>03</p>
+  <main class="w-full">
+    <div class="space-y-4 py-44">
+      <h2 class="text-3xl text-primary">成功完成訂單！</h2>
+      <h3 class="text-lg">前往<span class="text-primary">會員專區</span>查看訂單</h3>
+      <router-link to="/products" class="block text-lg">選購更多商品</router-link>
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts">
