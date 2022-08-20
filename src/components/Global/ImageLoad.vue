2faf1a4158b68ef7c9ea27fe82abc9b21268dcb4
JiangShuuu <globelex65@gmail.com>
2022-08-20 12:39:29 +0800

    upd image loading

--- src/components/Global/ImageLoad.vue
+++ src/components/Global/ImageLoad.vue
@@ -1,25 +1,30 @@
 <template>
-  <div class="loading-image">
-    <img @load="successLoadImg" :src="imageUrl" alt="載入圖片">
+  <div ref="imagebox" class="loading-image">
+    <img @load="successLoadImg" :src="imageUrl" alt="載入圖片" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
 defineProps<{ imageUrl: any }>()
 
+const imagebox = ref()
+
 const successLoadImg = () => {
-  event.target.parentElement.classList.remove('.loading-image')
+  imagebox.value.classList.remove('loading-image')
+  // event.target.parentElement.classList.remove('loading-image')
 }
 </script>
 
 <style lang="postcss" scoped>
 .loading-image {
-    height: 100%;
-    position: relative;
-    background-color: #FFF;
-    background-image: url(/image/loading.gif);
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: 30%;
+  height: 100%;
+  position: relative;
+  background-color: #fff;
+  background-image: url(/image/loading.gif);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 30%;
 }
 </style>
