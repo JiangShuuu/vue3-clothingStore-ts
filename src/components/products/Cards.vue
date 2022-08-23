a3eda9d97a1042dfb9793c0ca2f63e944fe51ba3
JiangShuuu <globelex65@gmail.com>
2022-08-23 21:38:43 +0800

    upd pinia cart function

--- src/components/products/Cards.vue
+++ src/components/products/Cards.vue
@@ -7,12 +7,12 @@
           <ImageLoad :image-url="arr.image" />
         </router-link>
         <div v-if="arr.isCart" class="absolute bottom-0 origin-bottom scale-y-0 opacity-0 cursor-pointer flex-center hover:bg-black"
-          :class="{ ani : arr.isOpen }" @click="deleteCart(arr)" >
+          :class="{ ani : arr.isOpen }" @click="mainUser.deleteCart(arr)" >
           <div class="h-10 p-2 bg-black w-44 md:w-56 opacity-30"></div>
           <span class="absolute text-white opacity-0" :class="{ showtext : arr.isOpen }">移除購物車</span>
         </div>
         <div v-else class="absolute bottom-0 origin-bottom scale-y-0 opacity-0 cursor-pointer flex-center hover:bg-black"
-          :class="{ ani : arr.isOpen }" @click="addCart(arr)" >
+          :class="{ ani : arr.isOpen }" @click="mainUser.addCart(arr)" >
           <div class="h-10 p-2 bg-black w-44 md:w-56 opacity-30"></div>
           <span class="absolute text-white opacity-0" :class="{ showtext : arr.isOpen }">加入購物車</span>
         </div>
@@ -47,30 +47,6 @@ const props = defineProps<{ cards: any }>()
 props.cards.forEach((element:any) => {
   element.isOpen = false
 })
-
-const addCart = async (product) => {
-  console.log('id', product.id)
-  console.log(mainUser.currentUser)
-
-  if (!mainUser.currentUser) {
-    toast.error('請先登入')
-    return route.push('/signIn')
-  }
-
-  const { data } = await userAPI.addCart(product.id)
-  console.log(data)
-
-  mainUser.carts.push(product)
-  product.isCart = true
-}
-
-const deleteCart = async (product) => {
-  const { data } = await userAPI.deleteCart(product.id)
-
-  mainUser.carts = mainUser.carts.filter(item => item.id !== product.id)
-  product.isCart = false
-}
-
 </script>
 
 <style lang="postcss" scoped>
--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,9 +1,36 @@
 import { defineStore } from 'pinia'
+import { useToast } from 'vue-toastification'
+import { useRouter } from 'vue-router'
 import usersAPI from '~/apis/user'
+
+const toast = useToast()
+const route = useRouter()
+
+interface Product {
+  Category: Object,
+  categoryId: number,
+  description: string,
+  id: number,
+  img: string,
+  isCart: boolean,
+  isFavorited: boolean,
+  isOpen: false,
+  og_price: number,
+  price: number,
+  short_intro: string,
+  title: string,
+  viewCounts: number
+}
+interface User {
+  currentUser: object|null,
+  carts: Product[],
+  isAuthenticated: boolean,
+  token: string
+}
 
 export const useUserStore = defineStore({
   id: 'user',
-  state: () => ({
+  state: ():User => ({
     currentUser: null,
     carts: [],
     isAuthenticated: false,
@@ -36,6 +63,27 @@ export const useUserStore = defineStore({
         this.revokeAuthentication()
         return false
       }
+    },
+    async addCart (product:Product) {
+      console.log('id', product.id)
+      console.log(this.currentUser)
+
+      if (!this.currentUser) {
+        toast.error('請先登入')
+        return route.push('/signIn')
+      }
+
+      const { data } = await usersAPI.addCart(product.id)
+      console.log(data)
+
+      this.carts.push(product)
+      product.isCart = true
+    },
+    async deleteCart (product:Product) {
+      const { data } = await usersAPI.deleteCart(product.id)
+
+      this.carts = this.carts.filter(item => item.id !== product.id)
+      product.isCart = false
     }
   }
 })
