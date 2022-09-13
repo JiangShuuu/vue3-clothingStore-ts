4e4e22277d807b44ab9faff7d3b048e20484dc0e
JiangShuuu <globelex65@gmail.com>
2022-09-13 16:36:54 +0800

    upd delete Order

--- src/components/member/Order.vue
+++ src/components/member/Order.vue
@@ -5,8 +5,8 @@
       尚未有訂單, 請至<router-link to="/products" class="text-2xl text-primary">商品區</router-link>選購
     </section>
   </main>
-  <main v-else>
-    <table class="table-auto">
+  <main v-else class="min-h-[400px]">
+    <table class="border-separate border-spacing-5">
       <thead>
         <tr>
           <th>id</th>
@@ -15,16 +15,20 @@
           <th>收件人地址</th>
           <th>訂單金額</th>
           <th>訂單創建日期</th>
+          <th>刪除訂單</th>
         </tr>
       </thead>
       <tbody v-for="(item, index) in orderData" :key="index">
         <tr>
-          <td>{{item.id}}</td>
-          <td>{{item.name}}</td>
-          <td>{{item.phone}}</td>
-          <td>{{item.address}}</td>
-          <td>{{item.total}}</td>
-          <td>{{dateFormat(item.createdAt)}}</td>
+          <td class="text-center align-middle">{{item.id}}</td>
+          <td class="text-center align-middle">{{item.name}}</td>
+          <td class="text-center align-middle">{{item.phone}}</td>
+          <td class="text-center align-middle">{{item.address}}</td>
+          <td class="text-center align-middle">{{item.total}}</td>
+          <td class="text-center align-middle">{{dateFormat(item.createdAt)}}</td>
+          <td class="text-center align-middle" @click="deleteOrder(item.id)">
+            <Icon icon="icon-park-outline:delete-five" class="w-5 h-5 ml-5 text-center text-gray-500 cursor-pointer" />
+          </td>
         </tr>
       </tbody>
     </table>
@@ -56,6 +60,17 @@ async function getOrder () {
     const { data } = await userAPI.getOrders()
     orderData.value = data.data.orders as OrderInfo
     console.log(orderData.value)
+  } catch (err) {
+    toast.error('系統錯誤請稍後再試')
+    console.log(err)
+  }
+}
+
+async function deleteOrder (id:number) {
+  try {
+    const { data } = await userAPI.deleteOrder(id)
+    orderData.value = orderData.value.filter((item:OrderInfo) => item.id !== id)
+    console.log(data)
   } catch (err) {
     toast.error('系統錯誤請稍後再試')
     console.log(err)
