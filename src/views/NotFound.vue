577f68dd40453978d894261a6765e193486882c9
JiangShuuu <globelex65@gmail.com>
2022-09-15 21:42:17 +0800

    upd 404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center justify-center px-2 py-20">
     <div class="flex-col mb-4 flex-center">
-      <img src="/image/40407.png" class="w-1/2 max-w-lg mb-6" />
+      <img src="/image/404.png" class="w-1/2 max-w-lg mb-10" />
       <h1 class="mb-3 text-xl text-center lg:text-4xl">找不到此頁面</h1>
       <p class="mb-3 text-base text-center lg:text-xl">您要搜尋的頁面可能已刪除、更改名稱或暫時不可瀏覽。</p>
     </div>
