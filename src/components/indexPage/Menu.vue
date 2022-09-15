f12a77dc598e32704d45ccd5495a714e5e49402b
JiangShuuu <globelex65@gmail.com>
2022-09-15 22:43:31 +0800

    add link

--- src/components/indexPage/Menu.vue
+++ src/components/indexPage/Menu.vue
@@ -1,23 +1,23 @@
 <template>
   <main class="grid w-full grid-rows-3 gap-2 px-3 md:grid-cols-3 md:grid-rows-1 md:px-0">
-    <div class="imgbox">
+    <router-link to="/products?categoryId=1" class="imgbox">
       <img class="object-cover h-[120%] w-full scale-100 hover:scale-110 ease-in-out duration-700"
         src="../../assets/Image/clothes.jpg" alt="">
       <li><i class="anicon fas fa-angle-double-down"></i></li>
       <span class="anispan">熱銷上衣</span>
-    </div>
-    <div class="imgbox">
+    </router-link>
+    <router-link to="/products?categoryId=3" class="imgbox">
       <img class="object-cover h-[120%] w-full scale-100 hover:scale-110 ease-in-out duration-700"
         src="../../assets/Image/coat.jpg" alt="">
       <li><i class="anicon fas fa-angle-double-down"></i></li>
       <span class="anispan">精選外套</span>
-    </div>
-    <div class="imgbox">
+    </router-link>
+    <router-link to="/products?categoryId=2" class="imgbox">
       <img class="object-cover h-[120%] w-full scale-100 hover:scale-110 ease-in-out duration-700"
         src="../../assets/Image/pants.jpg" alt="">
       <li><i class="anicon fas fa-angle-double-down"></i></li>
       <span class="anispan">經典褲款</span>
-    </div>
+    </router-link>
   </main>
 </template>
 
--- src/components/indexPage/ShopNow.vue
+++ src/components/indexPage/ShopNow.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="w-full px-3 mt-2 md:px-0 md:flex md:flex-row-reverse">
-    <div class="imgbox">
+    <router-link to="/products" class="imgbox">
       <img class="object-cover h-[120%] w-full scale-100 hover:scale-110 ease-in-out duration-700"
         src="../../assets/Image/shop01.jpg" alt="">
-    </div>
+    </router-link>
     <div class="flex-col my-5 space-y-2 flex-center md:flex-1">
       <span class="text-lg font-normol"> 質感日系時尚 </span>
       <div class="flex-col space-y-px flex-center">
@@ -11,9 +11,9 @@
         <span class="font-thin">我們希望創造一個優質空間</span>
         <span class="font-thin">讓每個前來的你們，都能穿出屬於自己的日系獨特風格。</span>
       </div>
-      <button
+      <router-link to="/products"
         class="px-4 py-1.5 font-thin border border-black hover:bg-primary hover:text-white duration-500 ease-in-out">SHOP
-        NOW</button>
+        NOW</router-link>
     </div>
   </main>
 </template>
