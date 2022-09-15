e0b364c36a8e2bc52fdec7fc08b463878d271fed
JiangShuuu <globelex65@gmail.com>
2022-09-15 16:15:35 +0800

    upd firebase

--- src/env.d.ts
+++ src/env.d.ts
@@ -18,7 +18,6 @@ interface ImportMetaEnv extends Readonly<Record<string, string | boolean | numbe
   readonly VITE_FIREBASE_PROJECT_ID: string,
   readonly VITE_FIREBASE_STORAGE_BUCKET: string,
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string,
-  readonly VITE_FIREBASE_APP_ID: string,
-  readonly VITE_FIREBASE_MEASUREMENT_ID: string,
+  readonly VITE_FIREBASE_APP_ID: string
   // more env variables...
 }
