e8dd74a05638e391540f6e820b011d709920aa6f
JiangShuuu <globelex65@gmail.com>
2022-09-22 21:01:32 +0800

    upd thirdpartylogin

--- src/components/signIn/FacebookSignIn.vue
+++ src/components/signIn/FacebookSignIn.vue
@@ -7,7 +7,12 @@
 import { firebaseApp } from '~/plugins/firebase'
 import { getAuth, FacebookAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 import { useToast } from 'vue-toastification'
+import { useUserStore } from '~/stores/user'
+import { useRouter } from 'vue-router'
+import userAPI from '~/apis/user'
 
+const mainStore = useUserStore()
+const route = useRouter()
 const toast = useToast()
 const firebaseAuth = getAuth(firebaseApp)
 const provider = new FacebookAuthProvider()
@@ -15,7 +20,27 @@ const provider = new FacebookAuthProvider()
 const signIn = () => {
   signInWithPopup(firebaseAuth, provider)
     .then((result) => {
-      console.log(result)
+      const user = result.user
+      // const accessToken = result.user.accessToken
+
+      userAPI.thirdPartyLogin(user)
+        .then((data) => {
+          // 將伺服器回傳的token 保存在 localStorage 中
+          localStorage.setItem('token', data.data.data.token)
+
+          // pinia user
+          mainStore.setCurrentUser(data.data.data)
+
+          // Toast
+          toast.success('登入成功', {
+            timeout: 1000
+          })
+          // 成功登入後進行轉址
+          route.push('/member')
+        })
+        .catch((err) => {
+          console.log(err)
+        })
     })
     .catch((err) => {
       console.log(err)
