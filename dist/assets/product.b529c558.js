066196d3ea8aa2ca37f0b1ac0c56e41ea9b515d5
JiangShuuu <globelex65@gmail.com>
2022-09-27 22:53:10 +0800

    build

--- dist/assets/product.b529c558.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./index.cb979ff8.js";const n={getProducts(r,t,s,a){const c=new URLSearchParams({page:r,categoryId:t,sort:s,value:a});return e.get(`/products?${c.toString()}`)},getProduct(r){return e.get(`/product/${r}`)},searchProducts(r){const t=new URLSearchParams({keyword:r});return e.get(`/products/search?${t.toString()}`)}};export{n as p};
