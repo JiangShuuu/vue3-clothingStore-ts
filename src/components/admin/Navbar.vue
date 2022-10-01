b86b7feb17d1882d26791d5dc328eb3bbeb61625
JiangShuuu <globelex65@gmail.com>
2022-10-01 15:35:54 +0800

    upd admin navbar

--- src/components/admin/Navbar.vue
+++ src/components/admin/Navbar.vue
@@ -1,1 +1,8 @@
-<template>後台管理</template>
+<template>
+  <main class="w-full border flex-center">
+    <div class="w-[200px]">
+      <img class="w-[200px]" src="/image/LogoBA.png" alt="" />
+    </div>
+    <h1 class="text-2xl">後台管理</h1>
+  </main>
+</template>
